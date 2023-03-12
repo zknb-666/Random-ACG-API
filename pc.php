b7a84fd1ea1b31629c7db11d66e59a619dcd4de3
zknb-666 <2865467965@qq.com>
2023-03-12 14:17:39 +0800

    use jihulab raw

--- pc.php
+++ pc.php
@@ -1,6 +1,6 @@
 <?php
 //读取文本
-$str = explode("\n", file_get_contents('https://fastly.jsdelivr.net/gh/zknb-666/wallhavenrequests@main/pc.txt'));
+$str = explode("\n", file_get_contents('https://jihulab.com/zknb-666/wallhavenrequests/-/raw/main/pc.txt'));
 $k = rand(0,count($str));
 $sina_img = str_re($str[$k]);
 
