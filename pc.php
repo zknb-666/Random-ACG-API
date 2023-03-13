9df422d8ca32f4c700a85c94a1d8118717d96d86
zknb-666 <2865467965@qq.com>
2023-03-14 06:37:25 +0800

    使用本地列表

--- pc.php
+++ pc.php
@@ -1,6 +1,6 @@
 <?php
 //读取文本
-$str = explode("\n", file_get_contents('https://jihulab.com/zknb-666/wallhavenrequests/-/raw/main/pc.txt'));
+$str = explode("\n", file_get_contents('pc.txt'));
 $k = rand(0,count($str));
 $sina_img = str_re($str[$k]);
 
--- pe.php
+++ pe.php
@@ -1,6 +1,6 @@
 <?php
 //读取文本
-$str = explode("\n", file_get_contents('https://jihulab.com/zknb-666/wallhavenrequests/-/raw/main/pe.txt'));
+$str = explode("\n", file_get_contents('pe.txt'));
 $k = rand(0,count($str));
 $sina_img = str_re($str[$k]);
 
