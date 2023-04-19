640ca1a63d3099129eff83f83fc2b1da6fc62f9d
zknb-666 <2865467965@qq.com>
2023-04-20 07:12:30 +0800

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>随机二次元图片API</title>
+		<meta name="keywords" content="图库,二次元图片,二次元API,动漫图片API,图片API">
+		<meta name="description" content="随机二次元图片API,随机动漫壁纸，每次不一样。">
+		<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<div class="container">
+			<p algin=“center” class="title">随机二次元图片API</p>
+			<div class="well">
+				<p>壁纸来源<a href="http://wallhaven.cc" target="_blank">wallhaven.cc</a></p>
+        		<p>每日排行榜更新</p>
+				<p>自判断二次元API基本调用格式：</p>
+				<pre><a href="<?php $_SERVER['HTTP_HOST'] ?>/api.php" target="_blank"><?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/api.php</a></pre>
+				<p>PC随机二次元API</p>
+				<pre><a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pc.php" target="_blank"><?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pc.php</a></pre>
+				<p>PE随机二次元API</p>
+				<pre><a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pe.php" target="_blank"><?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pe.php</a></pre>
+				<p>请求方式</p>
+				<pre>get/post</pre>
+				<p>返回格式</p>
+				<pre>json/images</pre>
+				<p>个人博客</p>
+				<pre><a href="https://blog.zknb-666.ml" target="_blank">https://blog.zknb-666.ml</a></pre>
+			</div>
+		</div>
+		<div class="bottom">
+                <p>Copyright © 2023 zknb</p>
+		</div>
+	</body>
+</html>
