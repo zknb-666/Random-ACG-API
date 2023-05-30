05a3ff963f102b9150243d2244c8366388bfc839
zknb-666 <80298711+zknb-666@users.noreply.github.com>
2023-05-30 10:41:57 +0000

    delet repldeploy

--- main.sh
+++ main.sh
@@ -2,5 +2,4 @@ echo Hello World
 URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
 while true; do curl -s "https://$URL" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
 chmod +x ./repl.deploy
-nohup ./repl.deploy --standalone php -S 0.0.0.0:8000 -t .
-
+nohup  php -S 0.0.0.0:8000 -t .
