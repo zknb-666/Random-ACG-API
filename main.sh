c5c52fc5d4f66549969f91376227c311a3bec29d
zknb-666 <80298711+zknb-666@users.noreply.github.com>
2023-06-07 12:07:46 +0000

    显示运行信息

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
 echo Hello World
 URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
 while true; do curl -s "https://$URL" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
-chmod +x ./repl.deploy
-nohup  php -S 0.0.0.0:8000 -t .
+php -S 0.0.0.0:8000 -t .
